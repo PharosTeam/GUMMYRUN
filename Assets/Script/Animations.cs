3e7c2a4da0e6cdf5970d5aed69d5fc76d526628e
Beben <elben9229@gmail.com>
2019-06-27 10:27:20 +0700

    27-06-19
    Update Slice

---
+++ Assets/Script/Animations.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Animations : MonoBehaviour
+{
+    public RectTransform rt;
+
+    //public void Click()
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.transform.localScale = new Vector3(0.9f, 0.9f, 1);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
