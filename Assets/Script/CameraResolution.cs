46de0d3dc464c1259c6b36471dbd7369cade6386
Beben <elben9229@gmail.com>
2019-07-05 17:18:15 +0700

    latest

--- Assets/Script/CameraResolution.cs
+++ Assets/Script/CameraResolution.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CameraResolution : MonoBehaviour
 {
     void Start()
-    {
+    { 
+        /*
         // set the desired aspect ratio (the values in this example are
         // hard-coded for 16:9, but you could make them into public
         // variables instead so you can set them at design time)
-        float targetaspect = 960f / 640f;
+        float targetaspect = 1920f / 1080f;
 
         // determine the game window's current aspect ratio
         float windowaspect = (float)Screen.width / (float)Screen.height;
@@ -44,6 +45,30 @@ public class CameraResolution : MonoBehaviour
             rect.y = 0;
 
             camera.rect = rect;
+        }
+        */
+
+        //test 2
+        
+        float TARGET_WIDTH = 1920.0f;
+        float TARGET_HEIGHT = 1080.0f;
+        int PIXELS_TO_UNITS = 30; // 1:1 ratio of pixels to units
+
+        float desiredRatio = TARGET_WIDTH / TARGET_HEIGHT;
+        float currentRatio = (float)Screen.width / (float)Screen.height;
+
+        if (currentRatio >= desiredRatio)
+        {
+            // Our resolution has plenty of width, so we just need to use the height to determine the camera size
+            Camera.main.orthographicSize = TARGET_HEIGHT / 4 / PIXELS_TO_UNITS;
+            Camera.main.orthographicSize = TARGET_WIDTH/ 4 / PIXELS_TO_UNITS;
+        }
+        else
+        {
+            // Our camera needs to zoom out further than just fitting in the height of the image.
+            // Determine how much bigger it needs to be, then apply that to our original algorithm.
+            float differenceInSize = desiredRatio / currentRatio;
+            Camera.main.orthographicSize = TARGET_HEIGHT / 4 / PIXELS_TO_UNITS * differenceInSize;
         }
     }
 }
